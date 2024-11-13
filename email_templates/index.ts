5fe8136b9878b711919e192cf2be1f752f44dea6
Prompt Logic GmbH <business@promptlogic.de>
2024-11-13 15:24:20 +0100

    reowrk email works finished

--- email_templates/index.ts
+++
@@ -1,8 +0,0 @@
-export const emailTemplates = {
-  passwordReset: require('./password-reset.html'),
-  verifyEmail: require('./verify-email.html'),
-  // Add other email templates here as needed
-  // welcomeEmail: require('./welcome-email.html'),
-} 
-
-
